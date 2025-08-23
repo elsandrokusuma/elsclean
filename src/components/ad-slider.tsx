64b37257380fe4f1aa022093852407858cbc0521
Deva Kusuma <elsandrokusuma@gmail.com>
2025-08-23 04:45:01 +0000

    ads tambahkan data base ke firestore image iklan salah satu ganti dengan

--- src/components/ad-slider.tsx
+++ src/components/ad-slider.tsx
@@ -25,7 +25,7 @@ const placeholderAds: Omit<AdData, 'id'>[] = [
     link: '#',
   },
   {
-    image: 'https://placehold.co/1200x400.png',
+    image: 'https://drive.google.com/uc?id=1OjPT2E_kw7Iwmxr_O_h5hDetAE7gWom_',
     imageHint: 'delivery service',
     title: 'Layanan Antar Jemput',
     description: 'Gratis antar jemput untuk wilayah Jakarta.',
