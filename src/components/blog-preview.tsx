3d645cd82684066a185d46b582d4c4a810a00ced
Deva Kusuma <elsandrokusuma@gmail.com>
2025-09-24 07:32:59 +0000

    versi desktop naikan ini sedikit agar gapnya tidak terlalu tinggi

--- src/components/blog-preview.tsx
+++ src/components/blog-preview.tsx
@@ -37,7 +37,7 @@ export default function BlogPreview() {
   }, []);
 
   return (
-    <section id="blog" className="w-full py-12 md:py-16">
+    <section id="blog" className="w-full py-12 md:pt-8 md:pb-16">
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
